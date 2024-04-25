95f6b6106861c7f444d130c892968d739157064a
Dylan Neff <Dyn0402@hotmail.com>
2024-04-25 13:22:36 +0200

    Didn't save hv_functions.h or Makefile

--- hv_c_lib/hv_functions.h
+++ hv_c_lib/hv_functions.h
@@ -5,14 +5,14 @@
 
 #include "CAENHVWrapper.h"
 
-int *log_in(const char* ip_address, const char* username, const char* password);
-void get_crate_map(int* sys_handle);
-void log_out(int* sys_handle);
+int log_in(const char* ip_address, const char* username, const char* password);
+void get_crate_map(int sys_handle);
+void log_out(int sys_handle);
 
-int get_ch_power(int* sys_handle, int slot, int chan);
+int get_ch_power(int sys_handle, int slot, int chan);
 
-float get_ch_vmon(int* sys_handle, int slot, int chan);
-void set_ch_v0(int* sys_handle, int slot, int chan, int value);
+float get_ch_vmon(int sys_handle, int slot, int chan);
+void set_ch_v0(int sys_handle, int slot, int chan, int value);
 
 
 #endif
