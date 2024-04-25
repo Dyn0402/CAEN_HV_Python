31c17e38fb7ebd8aa568688ff63a35eee7461f1d
Dylan Neff <Dyn0402@hotmail.com>
2024-04-25 15:29:14 +0200

    Reworked HVPyWrapper.py a bit to make more friendly

--- HVPyWrapper.py
+++ HVPyWrapper.py
@@ -12,66 +12,84 @@ import ctypes
 
 
 class HVPyWrapper:
-    def __init__(self, library_path):
+    """
+    Wrapper class for the CAEN HV C library. This class loads the shared library and defines the function prototypes
+    Needed to call the functions in the shared library. The class has methods that call the C functions with the proper
+    parameters and return the results.
+
+    Need to make a call within 15 seconds of the last, otherwise the connection will be lost and the sys_handle will be
+    invalid. This is a limitation of the C library.
+    """
+    def __init__(self, library_path, ip_address, username, password):
+        self.library_path = library_path
+        self.ip_address = ip_address
+        self.username = username
+        self.password = password
+        self.sys_handle = None
+
+    def __enter__(self):
         # Load the shared library
-        self.library = ctypes.CDLL(library_path)
+        self.library = ctypes.CDLL(self.library_path)
+        self.sys_handle = self.log_in_wrapper()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.sys_handle is not None:
+            self.log_out_wrapper()
+            self.sys_handle = None
+
+    def log_in_wrapper(self):
+        # Convert Python strings to bytes
+        ip_bytes = self.ip_address.encode('utf-8')
+        username_bytes = self.username.encode('utf-8')
+        password_bytes = self.password.encode('utf-8')
 
         # Define the function prototype for log_in
-        self.log_in = self.library.log_in
-        self.log_in.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
-        self.log_in.restype = ctypes.c_int
-
-        # Define the function prototype for log_out
-        self.log_out = self.library.log_out
-        self.log_out.argtypes = [ctypes.c_int]
-
-        # Define the function prototype for get_ch_power
-        self.get_ch_power = self.library.get_ch_power
-        self.get_ch_power.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
-
-        # Define the function prototype for get_ch_vmon
-        self.get_ch_vmon = self.library.get_ch_vmon
-        self.get_ch_vmon.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
-        self.get_ch_vmon.restype = ctypes.c_float
-
-        # Define the function prototype for set_ch_v0
-        self.set_ch_v0 = self.library.set_ch_v0
-        self.set_ch_v0.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_float]
-
-        # Define the function prototype for set_ch_pw
-        self.set_ch_pw = self.library.set_ch_pw
-        self.set_ch_pw.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
-
-    def log_in_wrapper(self, ip_address, username, password):
-        # Convert Python strings to bytes
-        ip_bytes = ip_address.encode('utf-8')
-        username_bytes = username.encode('utf-8')
-        password_bytes = password.encode('utf-8')
+        log_in = self.library.log_in
+        log_in.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+        log_in.restype = ctypes.c_int
 
         # Call the C function with the bytes and integer parameters
-        sys_handle = self.log_in(ip_bytes, username_bytes, password_bytes)
+        sys_handle = log_in(ip_bytes, username_bytes, password_bytes)
 
         # Return the result value
         return sys_handle
 
-    def log_out_wrapper(self, sys_handle):
-        # Call the C function with the integer parameter
-        self.log_out(sys_handle)
+    def log_out_wrapper(self):
+        # Define the function prototype for log_out
+        log_out = self.library.log_out
+        log_out.argtypes = [ctypes.c_int]
 
-    def get_ch_power_wrapper(self, sys_handle, slot, channel):
         # Call the C function with the integer parameter
-        power = self.get_ch_power(sys_handle, slot, channel)
+        log_out(self.sys_handle)
+
+    def get_ch_power_wrapper(self, slot, channel):
+        # Define the function prototype for get_ch_power
+        get_ch_power = self.library.get_ch_power
+        get_ch_power.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
+        # Call the C function with the integer parameter
+        power = get_ch_power(self.sys_handle, slot, channel)
         return power
 
-    def get_ch_vmon_wrapper(self, sys_handle, slot, channel):
+    def get_ch_vmon_wrapper(self, slot, channel):
+        # Define the function prototype for get_ch_vmon
+        get_ch_vmon = self.library.get_ch_vmon
+        get_ch_vmon.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int]
+        get_ch_vmon.restype = ctypes.c_float
         # Call the C function with the integer parameter
-        vmon = self.get_ch_vmon(sys_handle, slot, channel)
+        vmon = get_ch_vmon(self.sys_handle, slot, channel)
         return vmon
 
-    def set_ch_v0_wrapper(self, sys_handle, slot, channel, voltage):
+    def set_ch_v0_wrapper(self, slot, channel, voltage):
+        # Define the function prototype for set_ch_v0
+        set_ch_v0 = self.library.set_ch_v0
+        set_ch_v0.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_float]
         # Call the C function with the integer parameter
-        self.set_ch_v0(sys_handle, slot, channel, voltage)
+        set_ch_v0(self.sys_handle, slot, channel, voltage)
 
-    def set_ch_pw_wrapper(self, sys_handle, slot, channel, pw):
+    def set_ch_pw_wrapper(self, slot, channel, pw):
+        # Define the function prototype for set_ch_pw
+        set_ch_pw = self.library.set_ch_pw
+        set_ch_pw.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
         # Call the C function with the integer parameter
-        self.set_ch_pw(sys_handle, slot, channel, pw)
+        set_ch_pw(self.sys_handle, slot, channel, pw)
--- main.py
+++ main.py
@@ -22,39 +22,39 @@ def main():
     channels = [0, 1, 2, 3, 4]
     v0s = [50, 100, 150, 200, 250]
 
-    hv_c_lib = HVPyWrapper(lib_path)
-    sys_handle = hv_c_lib.log_in_wrapper(ip_address, username, password)
-    print(f'sys_handle: {sys_handle}')
+    with HVPyWrapper(lib_path, ip_address, username, password) as hv_wrapper:
+        print('Turning off channels')
+        for channel in channels:
+            hv_wrapper.set_ch_pw_wrapper(slot, channel, 0)
+            sleep(1)
 
-    print('Turning off channels')
-    for channel in channels:
-        hv_c_lib.set_ch_pw_wrapper(sys_handle, slot, channel, 0)
-        sleep(1)
+        sleep(5)
 
-    # print('Setting channels V0')
-    # for channel, v0 in zip(channels, v0s):
-    #     hv_c_lib.set_ch_v0_wrapper(sys_handle, slot, channel, v0)
-    #     sleep(1)
+        print('Setting channels V0')
+        for channel, v0 in zip(channels, v0s):
+            hv_wrapper.set_ch_v0_wrapper(slot, channel, v0)
+            sleep(1)
 
-    print('Turning on channels')
-    for channel in channels:
-        hv_c_lib.set_ch_pw_wrapper(sys_handle, slot, channel, 1)
-        sleep(1)
+        sleep(5)
 
-    sleep(10)
+        print('Turning on channels')
+        for channel in channels:
+            hv_wrapper.set_ch_pw_wrapper(slot, channel, 1)
+            sleep(1)
 
-    print('Getting channel power and Vmon')
-    for channel in channels:
-        power = hv_c_lib.get_ch_power_wrapper(sys_handle, slot, channel)
-        vmon = hv_c_lib.get_ch_vmon_wrapper(sys_handle, slot, channel)
-        print(f'Channel {channel} power: {power} Vmon: {vmon}')
+        sleep(10)
 
-    sleep(5)
+        print('Getting channel power and Vmon')
+        for channel in channels:
+            power = hv_wrapper.get_ch_power_wrapper(slot, channel)
+            vmon = hv_wrapper.get_ch_vmon_wrapper(slot, channel)
+            print(f'Channel {channel} power: {power} Vmon: {vmon}')
 
-    print('Turning off channels')
-    for channel in channels:
-        hv_c_lib.set_ch_pw_wrapper(sys_handle, slot, channel, 0)
-    hv_c_lib.log_out_wrapper(sys_handle)
+        sleep(5)
+
+        print('Turning off channels')
+        for channel in channels:
+            hv_wrapper.set_ch_pw_wrapper(slot, channel, 0)
 
     print('donzo')
 
